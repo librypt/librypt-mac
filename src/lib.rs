54f2bfa270a0721f3c73b3d8851abbc03a53e120
gc2004 <55647897+undersquire@users.noreply.github.com>
2023-01-06 00:21:10 -0800

    Rename trait functions.

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,19 @@ use std::error::Error;
 /// Represents a Message Authentication Code that can be attached to a message.
 pub type Mac<const LENGTH: usize> = [u8; LENGTH];
 
-/// Represents an algorithm for generating a MAC given a message and a `Secret`.
+/// Represents an algorithm for computing a MAC given a message and a `Secret`.
 ///
-/// NOTE: Make sure to consult documentation for specific MAC algorithms, as some may require providing more data than what the generate functions require to be secure.
+/// NOTE: Make sure to consult documentation for specific MAC algorithms, as some may require providing more data than what the compute functions require to be secure.
 pub trait MacAlgorithm<const LENGTH: usize> {
     type MacError: Error;
 
-    /// Attempt to generate a MAC given a message and a secret.
-    fn try_generate(&self, msg: &[u8], secret: &[u8]) -> Result<Mac<LENGTH>, Self::MacError>;
+    /// Attempt to compute a MAC given a message and a secret.
+    fn try_compute(&self, msg: &[u8], secret: &[u8]) -> Result<Mac<LENGTH>, Self::MacError>;
 
-    /// Generate a MAC given a message and a secret.
+    /// Compute a MAC given a message and a secret.
     ///
-    /// NOTE: This function will panic if the generation fails. See `try_generate` for a version with error handling.
-    fn generate(&self, msg: &[u8], secret: &[u8]) -> Mac<LENGTH> {
-        self.try_generate(msg, secret).unwrap()
+    /// NOTE: This function will panic if the computation fails. See `try_compute` for a version with error handling.
+    fn compute(&self, msg: &[u8], secret: &[u8]) -> Mac<LENGTH> {
+        self.try_compute(msg, secret).unwrap()
     }
 }
