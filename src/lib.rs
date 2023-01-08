f0d3789b3e616e78e36b0996dd252e553be462df
gc2004 <55647897+undersquire@users.noreply.github.com>
2023-01-07 19:04:34 -0800

    Simplified traits and types.

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,10 @@
-/// Represents the raw bytes of a Message Authentication Code (MAC).
+/// A Message Authentication Code (MAC) represented by a fixed array of bytes.
 pub type Mac<const SIZE: usize> = [u8; SIZE];
 
-/// Represents an algorithm for computing a MAC given a message and a `Secret`.
+/// A function for computing a MAC given a message and a `Secret`.
 ///
 /// NOTE: Make sure to consult documentation for specific MAC algorithms, as some may require providing more data than what the compute functions require to be secure.
-pub trait MacAlgorithm<const OUTPUT_SIZE: usize> {
-    /// Initialize MAC state.
-    fn new(secret: &[u8]) -> Self;
-
-    /// Update the input state of the MAC with the provided data.
-    fn update(&mut self, data: &[u8]);
-
-    /// Compute the final MAC with the MAC state.
-    fn finalize(self) -> Mac<OUTPUT_SIZE>;
-
-    /// Compute the final MAC with the MAC state, clearing the state for reuse.
-    fn finalize_reset(&mut self) -> Mac<OUTPUT_SIZE>;
-
+pub trait MacFn<const OUTPUT_SIZE: usize> {
     /// Compute a MAC given a message and a secret.
     fn compute(msg: &[u8], secret: &[u8]) -> Mac<OUTPUT_SIZE>;
 }
