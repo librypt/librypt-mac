5243adea3d1ebbf0fa5c6be43a3281697dd31fb3
gc2004 <55647897+undersquire@users.noreply.github.com>
2023-01-07 17:43:11 -0800

    Added some trait function requirements to Mac.

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,22 @@
-use std::error::Error;
-
-/// Represents a Message Authentication Code that can be attached to a message.
-pub type Mac<const SIZE: usize> = [u8; SIZE];
+/// Represents the raw bytes of a Message Authentication Code (MAC).
+pub type MacOutput<const SIZE: usize> = [u8; SIZE];
 
 /// Represents an algorithm for computing a MAC given a message and a `Secret`.
 ///
 /// NOTE: Make sure to consult documentation for specific MAC algorithms, as some may require providing more data than what the compute functions require to be secure.
-pub trait MacAlgorithm<const OUTPUT_SIZE: usize> {
-    /// Custom error type for the MAC algorithm.
-    type MacError: Error;
+pub trait Mac<const OUTPUT_SIZE: usize> {
+    /// Initialize MAC state.
+    fn new(secret: &[u8]) -> Self;
 
-    /// Attempt to compute a MAC given a message and a secret.
-    fn try_compute(&self, msg: &[u8], secret: &[u8]) -> Result<Mac<OUTPUT_SIZE>, Self::MacError>;
+    /// Update the input state of the MAC with the provided data.
+    fn update(&mut self, data: &[u8]);
+
+    /// Compute the final MAC with the MAC state.
+    fn finalize(self) -> MacOutput<OUTPUT_SIZE>;
+
+    /// Compute the final MAC with the MAC state, clearing the state for reuse.
+    fn finalize_reset(&mut self) -> MacOutput<OUTPUT_SIZE>;
 
     /// Compute a MAC given a message and a secret.
-    ///
-    /// NOTE: This function will panic if the computation fails. See `try_compute` for a version with error handling.
-    fn compute(&self, msg: &[u8], secret: &[u8]) -> Mac<OUTPUT_SIZE> {
-        self.try_compute(msg, secret).unwrap()
-    }
+    fn compute(&self, msg: &[u8], secret: &[u8]) -> MacOutput<OUTPUT_SIZE>;
 }
