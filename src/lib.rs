09eed941f83c0cc3dc3b5b3a331901f0d3b62950
gc2004 <55647897+undersquire@users.noreply.github.com>
2023-01-07 17:57:57 -0800

    Slight renaming and changes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 /// Represents the raw bytes of a Message Authentication Code (MAC).
-pub type MacOutput<const SIZE: usize> = [u8; SIZE];
+pub type Mac<const SIZE: usize> = [u8; SIZE];
 
 /// Represents an algorithm for computing a MAC given a message and a `Secret`.
 ///
 /// NOTE: Make sure to consult documentation for specific MAC algorithms, as some may require providing more data than what the compute functions require to be secure.
-pub trait Mac<const OUTPUT_SIZE: usize> {
+pub trait MacAlgorithm<const OUTPUT_SIZE: usize> {
     /// Initialize MAC state.
     fn new(secret: &[u8]) -> Self;
 
@@ -12,11 +12,11 @@ pub trait Mac<const OUTPUT_SIZE: usize> {
     fn update(&mut self, data: &[u8]);
 
     /// Compute the final MAC with the MAC state.
-    fn finalize(self) -> MacOutput<OUTPUT_SIZE>;
+    fn finalize(self) -> Mac<OUTPUT_SIZE>;
 
     /// Compute the final MAC with the MAC state, clearing the state for reuse.
-    fn finalize_reset(&mut self) -> MacOutput<OUTPUT_SIZE>;
+    fn finalize_reset(&mut self) -> Mac<OUTPUT_SIZE>;
 
     /// Compute a MAC given a message and a secret.
-    fn compute(&self, msg: &[u8], secret: &[u8]) -> MacOutput<OUTPUT_SIZE>;
+    fn compute(msg: &[u8], secret: &[u8]) -> Mac<OUTPUT_SIZE>;
 }
