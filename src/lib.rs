882a551449754fab732d9c63039bf2ef626b2ba8
gc2004 <55647897+undersquire@users.noreply.github.com>
2023-01-11 18:07:13 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,25 @@ pub type Mac<const SIZE: usize> = [u8; SIZE];
 /// A function for computing a MAC given a message and a `Secret`.
 ///
 /// NOTE: Make sure to consult documentation for specific MAC algorithms, as some may require providing more data than what the compute functions require to be secure.
-pub trait MacFn<const OUTPUT_SIZE: usize> {
+pub trait MacFn<const OUTPUT_SIZE: usize>: Sized {
+    /// Initialize MAC state.
+    fn new(secret: &[u8]) -> Self;
+
+    /// Update the MAC state with the given data.
+    fn update(&mut self, data: &[u8]);
+
+    /// Consumes the MAC state, producing the final MAC.
+    fn finalize(self) -> Mac<OUTPUT_SIZE>;
+
+    /// Produces the final MAC, resetting the MAC state for reuse.
+    fn finalize_reset(&mut self) -> Mac<OUTPUT_SIZE>;
+
     /// Compute a MAC given a message and a secret.
-    fn compute(msg: &[u8], secret: &[u8]) -> Mac<OUTPUT_SIZE>;
+    fn mac(msg: &[u8], secret: &[u8]) -> Mac<OUTPUT_SIZE> {
+        let mut mac = Self::new(secret);
+
+        mac.update(msg);
+
+        mac.finalize()
+    }
 }
